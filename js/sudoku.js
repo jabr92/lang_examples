b81ffa5b31502af49765fceb9469193a3ccec719
John Arin Ransom <jabr92@gmail.com>
2016-07-03 23:48:03 -0700

    js sudoku in progress

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,3 +1,6 @@
+total_cells = 81;
+possible_vals = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 var row_num = function(cell_num) {
     return Math.floor(cell_num / 9)
 }
@@ -10,37 +13,55 @@ var box_num = function(cell_num) {
 
 var solve_sudoku = function(puzzle_string) {
 
-    var possibles = [];
+    // build arrays for all units (rows, columns, and boxes)
     var rows = [];
-
-    // build arrays for rows, columns, and boxes
     for (i = 0; i < 9; i++) {
-        rows.push(".........")
+        rows.push(".........");
+    }
+    var columns = rows.slice();
+    var boxes = rows.slice();
+
+    var set_val = function(cell_num, val) {
+        r = row_num(cell_num);
+        c = col_num(cell_num);
+        rows[r][c] = val;
+        columns[c][r] = val;
+        boxes[box_num(cell_num)][c % 3 + 3 * (r % 3)] = val;
     }
 
-    var columns = rows.slice()
-    var boxes = rows.slice()
-
-    for (i = 0; i < 81; i++) {
-        val = puzzle_string[i]
+    // add known values to units
+    for (i = 0; i < total_cells; i++) {
+        val = puzzle_string[i];
         if (val != ".") {
-            r = row_num(i)
-            c = col_num(i)
-            rows[r][c] = val
-            columns[c][r] = val
-            boxes[box_num(i)][c % 3 + 3 * (r % 3)] = val
-        }
-        possibles.push(val)
-
-
-    }
-
-    for (i = 0;i < 81; i++) {
-        if (possibles[i] == ".") {
-
+            set_val(i, val);
         }
     }
-    return possibles
+
+    var get_val = function(cell_num){
+        r = row_num(cell_num);
+        c = col_num(cell_num);
+        return rows[r][c]
+    }
+
+    var get_units = function(cell_num){
+        r = row_num(cell_num);
+        c = col_num(cell_num);
+        b = box_num(cell_num);
+        return [rows[r], columns[c], boxes[b]]
+    }
+
+    var scan_values = function() {
+        for (i = 0; i < total_cells; i++) {
+            if (get_val(i) == ".") {
+                possible_vals.forEach(function checkDetermined(val){
+
+                })
+            }
+        }
+    }
+
+
+
 }
 
 test = ".................85.7.1..2...........3...6.....9..523..6...3.1...18...54.4.69...7"
